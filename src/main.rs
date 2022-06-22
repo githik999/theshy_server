7b8c690b909c70347ff24352c303ceaff8df8c11
chris <kakaxi77749@gmail.com>
2022-06-22 14:35:53 +0800

    pub

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use lucian::gate::hub::line_header::LineType;
-use lucian::log::Log;
 use lucian::server::Server;
-
-const APP_PORT:usize = 3389;
-const LOG_PORT:usize = 6000;
+use omg_cool::{log::Log, config::Config,header::LineType};
 
 fn main() {
-    Log::init();
-    let mut app = Server::new(APP_PORT,LineType::Operator);
-    let mut http = Server::new(LOG_PORT,LineType::Http);
+    Log::create_log_dir();
+    Config::set_panic_hook();
+    let (app_addr,http_addr,write_log) = Config::get_listen_addr();
+    if write_log {
+        Config::turn_on();
+    }
+    let mut app = Server::new(app_addr,LineType::Operator);
+    let mut http = Server::new(http_addr,LineType::Http);
     std::thread::spawn(move ||{
         http.start();
     });
     app.start();
 }
-
